5a19c711fc23e5fa82de0e65becee3173f587b4f
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-12-27 11:19:32 +0530

    rest api

---
+++ movieRest/src/test/kotlin/com/example/movieRest/MovieRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.movieRest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
